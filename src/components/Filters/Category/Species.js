cef92415c3ca61c147d72db04268bdeedc0a2760
Gmatieso <matiesogeoffrey@gmail.com>
2024-04-17 14:44:52 +0300

    Setting Filters for Species done

--- src/components/Filters/Category/Species.js
+++ src/components/Filters/Category/Species.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import FilterBTN from './FilterBTN'
 
-const Species = () => {
+const Species = ({setPageNumber, setSpecies}) => {
     let species = [
         "Human",
         "Alien",
@@ -22,8 +22,15 @@ const Species = () => {
                 </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                {species.map((items, index) =>  (<FilterBTN key={index} name = "species" index = {index} items = {items}/>))}
+            <div className="accordion-body d-flex flex-wrap gap-3">
+                {species.map((items, index) =>  
+                (<FilterBTN 
+                    setPageNumber = {setPageNumber} 
+                    task={setSpecies} 
+                    key={index} 
+                    name = "species" 
+                    index = {index} 
+                    items = {items}/>))}
                 </div>
             </div>
         </div>
