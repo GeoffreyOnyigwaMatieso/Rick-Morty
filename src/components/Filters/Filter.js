f77c07afb76c4b027d585e10eb35ac97a3c0dba3
Gmatieso <matiesogeoffrey@gmail.com>
2024-04-18 01:26:14 +0300

    Filter Component Done

--- src/components/Filters/Filter.js
+++ src/components/Filters/Filter.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import Gender from './Category/Gender'
-import Species from './Category/Species'
-import Status from './Category/Status'
+import React from "react";
+import Gender from "./category/Gender";
+import Species from "./category/Species";
+import Status from "./Category/Status";
+// import Status from "./category/Status";
 
-const Filter = ({setSpecies, setStatus, setPageNumber, setGender}) => {
+const Filter = ({
+  pageNumber,
+  updatePageNumber,
+  updateStatus,
+  updateGender,
+  updateSpecies,
+}) => {
+  let clear = () => {
+    updateStatus("");
+    updateGender("");
+    updateSpecies("");
+    updatePageNumber(1);
+    window.location.reload(false);
+  };
+  return (
+    <div className="col-lg-3 col-12 mb-5">
+      <div className="text-center fw-bold fs-4 mb-2">Filters</div>
+      <div
+        style={{ cursor: "pointer" }}
+        onClick={clear}
+        className="text-primary text-decoration-underline text-center mb-3"
+      >
+        Clear Filters
+      </div>
+      <div className="accordion" id="accordionExample">
+        <Status
+          updatePageNumber={updatePageNumber}
+          updateStatus={updateStatus}
+        />
+        <Species
+          updatePageNumber={updatePageNumber}
+          updateSpecies={updateSpecies}
+        />
+        <Gender
+          updatePageNumber={updatePageNumber}
+          updateGender={updateGender}
+        />
+      </div>
+    </div>
+  );
+};
 
-    let clear = () => { 
-        setSpecies('')
-        setStatus('')
-        setGender('')
-        setPageNumber('')
-        window.location.reload(false)
-    }
-
-    return (
-        <div className='col-3'>
-            <div className='text-center fw-bold fs-4 mb-2'>Filter </div>
-            <div 
-             onClick = {clear}
-             style={{ cursor: "pointer" }} 
-             className='text-center text-primary text-decoration-underline mb-4'
-             >
-                Clear Filter
-            </div>
-
-            <div className="accordion" id="accordionExample">
-                <Status setPageNumber = {setPageNumber} setStatus = {setStatus} />
-                <Species setPageNumber = {setPageNumber} setSpecies = {setSpecies}/>
-                <Gender setGender = {setGender} setPageNumber = {setPageNumber}/>
-            </div>
-
-        </div>
-    )
-}
-
-export default Filter
+export default Filter;
