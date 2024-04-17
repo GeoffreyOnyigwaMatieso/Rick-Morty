15066819a864816e9acd7890d84c5e0e70206f9d
Gmatieso <matiesogeoffrey@gmail.com>
2024-04-17 12:23:12 +0300

    Setting Filter for Gender done

--- src/components/Filters/Category/Gender.js
+++ src/components/Filters/Category/Gender.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import FilterBTN from './FilterBTN'
 
-const Gender = () => {
+const Gender = ({setGender, setPageNumber}) => {
     let genders = ["female", "male", "genderless", "unknown"]
     return (
         <div className="accordion-item">
             <h2 className="accordion-header">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <button 
+                className="accordion-button collapsed" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#collapseOne" 
+                aria-expanded="true" 
+                aria-controls="collapseOne">
                     Gender
                 </button>
             </h2>
-            <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                {genders.map((items, index) =>  (<FilterBTN key={index} name = "gender" index = {index} items = {items}/>))}
+            <div id="collapseOne" className="accordion-collapse collapse " data-bs-parent="#accordionExample">
+            <div className="accordion-body d-flex flex-wrap gap-3">
+                {
+                    genders.map((items, index) =>  ( 
+
+                    <FilterBTN 
+                    task = {setGender}
+                    setPageNumber = {setPageNumber} 
+                    setGender = {setGender} 
+                    key={index} 
+                    name = "gender" 
+                    index = {index} 
+                    items = {items}
+
+                    />))}
 
                 </div>
             </div>
