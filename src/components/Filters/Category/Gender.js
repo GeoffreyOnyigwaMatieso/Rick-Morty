3a0d7e7b5805511fdaa6e2d113a0c07947b9a019
Gmatieso <matiesogeoffrey@gmail.com>
2024-04-18 01:09:07 +0300

    Gender Component Done

--- src/components/Filters/Category/Gender.js
+++ src/components/Filters/Category/Gender.js
@@ -1,41 +1,45 @@
-import React from 'react'
-import FilterBTN from './FilterBTN'
+import React from "react";
+import FilterBTN from "../FilterBTN";
 
-const Gender = ({setGender, setPageNumber}) => {
-    let genders = ["female", "male", "genderless", "unknown"]
-    return (
-        <div className="accordion-item">
-            <h2 className="accordion-header">
-                <button 
-                className="accordion-button collapsed" 
-                type="button" 
-                data-bs-toggle="collapse" 
-                data-bs-target="#collapseOne" 
-                aria-expanded="true" 
-                aria-controls="collapseOne">
-                    Gender
-                </button>
-            </h2>
-            <div id="collapseOne" className="accordion-collapse collapse " data-bs-parent="#accordionExample">
-            <div className="accordion-body d-flex flex-wrap gap-3">
-                {
-                    genders.map((items, index) =>  ( 
-
-                    <FilterBTN 
-                    task = {setGender}
-                    setPageNumber = {setPageNumber} 
-                    setGender = {setGender} 
-                    key={index} 
-                    name = "gender" 
-                    index = {index} 
-                    items = {items}
-
-                    />))}
-
-                </div>
-            </div>
+const Gender = ({ updateGender, updatePageNumber }) => {
+  let genders = ["female", "male", "genderless", "unknown"];
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" id="headingThree">
+        <button
+          className="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseThree"
+          aria-expanded="false"
+          aria-controls="collapseThree"
+        >
+          Gender
+        </button>
+      </h2>
+      <div
+        id="collapseThree"
+        className="accordion-collapse collapse"
+        aria-labelledby="headingThree"
+        data-bs-parent="#accordionExample"
+      >
+        <div className="accordion-body d-flex flex-wrap gap-3">
+          {genders.map((items, index) => {
+            return (
+              <FilterBTN
+                name="gender"
+                index={index}
+                key={index}
+                updatePageNumber={updatePageNumber}
+                task={updateGender}
+                input={items}
+              />
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Gender
+export default Gender;
