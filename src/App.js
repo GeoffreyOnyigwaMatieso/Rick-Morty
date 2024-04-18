ad1a0081006001ca74460b59efb0705cfa1c96d2
Gmatieso <matiesogeoffrey@gmail.com>
2024-04-18 03:22:20 +0300

    Code Refactored

--- src/App.js
+++ src/App.js
@@ -1,77 +1,83 @@
-import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+import Search from "./components/Search/Search";
+import Card from "./components/Cards/Cards";
+import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filters/Filter";
-import Cards from "./components/Cards/Cards";
-import Pagination from './components/Pagination/Pagination';
-import Search from './components/Search/Search';
-import Navbar from './components/NavBar/Navbar';
+import Navbar from "./components/NavBar/Navbar";
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
-import Episodes from './Pages/Episodes';
-import Location from './Pages/Location';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
+import CardDetails from "./components/Cards/CardDetails";
 
 function App() {
   return (
     <Router>
-      <div className = "App">
+      <div className="App">
         <Navbar />
       </div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/episodes" element={<Episodes/>}/>
-        <Route path="/location" element={<Location/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
+
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<CardDetails />} />
+
+        <Route path="/location" element={<Location />} />
+        <Route path="/location/:id" element={<CardDetails />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-const Home = ()=> {
+const Home = () => {
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
+  let [fetchedData, updateFetchedData] = useState([]);
+  let [search, setSearch] = useState("");
+  let { info, results } = fetchedData;
 
-  let [pageNumber, setPageNumber] = useState(1)
-  let [fetchData, updateFetchData] = useState([])
-  let [search, setSearch] = useState('')
-  let [status, setStatus] = useState('')
-  let [gender, setGender] = useState('')
-  let [species, setSpecies] = useState('')
-
-  // destructure results for Cards and info for pagination
-  let { info, results } = fetchData
-
-  // console.log(results)
   let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
-  
 
   useEffect(() => {
     (async function () {
-      let data = await fetch(api).then(res => res.json())
-      updateFetchData(data)
-    })()
-  }, [api])
-
-
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
   return (
     <div className="App">
-        <Search setPageNumber = {setPageNumber} setSearch = {setSearch}/>
+      <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
-            <Filter
-            setSpecies = {setSpecies} 
-            setGender = {setGender} 
-            setStatus = {setStatus}
-            setPageNumber = {setPageNumber}
-             />
-          <div className="col-8">
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
+          <div className="col-lg-8 col-12">
             <div className="row">
-              <Cards results={results} />
+              <Card page="/" results={results} />
             </div>
           </div>
         </div>
       </div>
-      <Pagination info = {info} pageNumber = {pageNumber} setPageNumber = {setPageNumber}></Pagination>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
     </div>
   );
-}
+};
 
-export default Home;
+export default App;
